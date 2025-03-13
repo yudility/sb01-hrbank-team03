ce380826ffbc25b25d2fc448c1fb00a54bec5581
yudility <yudility@gmail.com>
2025-03-13 17:19:39 +0900

    initialize

---
+++ src/main/java/com/sprint/part2/sb1hrbankteam03/Sb1HrbankTeam03Application.java
@@ -0,0 +1,13 @@
+package com.sprint.part2.sb1hrbankteam03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb1HrbankTeam03Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Sb1HrbankTeam03Application.class, args);
+  }
+
+}
---
+++ src/test/java/com/sprint/part2/sb1hrbankteam03/Sb1HrbankTeam03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprint.part2.sb1hrbankteam03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb1HrbankTeam03ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
